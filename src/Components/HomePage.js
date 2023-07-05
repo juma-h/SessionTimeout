a9852e7d944bb55827e5768b17f16830f80a1277
Juma-h <mitchfibi@gmail.com>
2023-07-05 13:12:14 +0300

    css

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,26 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useIdle from "../Hooks/useIdleTimer.js";
+import "./component.css"
 
 function HomePage() {
+
+
   const navigate = useNavigate();
   const [showModal, setShowModal] = useState(false);
   const [remainingTime, setRemainingTime] = useState(0);
 
   const handleIdle = () => {
-    setShowModal(true);//show modal
-    setRemainingTime(15);//set 15 seconds as time remaining
+    setShowModal(true); //show modal
+    setRemainingTime(30); //set 15 seconds as time remaining
   };
 
-  const { isIdle } = useIdle({ onIdle: handleIdle, idleTime: 0.5 });
+  const { isIdle } = useIdle({ onIdle: handleIdle, idleTime: 0.3});
+  console.log("is user idle?", isIdle);
 
   useEffect(() => {
     let interval;
 
     if (isIdle && showModal) {
       interval = setInterval(() => {
-        setRemainingTime((prevRemainingTime) =>
-          prevRemainingTime > 0 ? prevRemainingTime - 1 : 0//reduces the second by 1
+        setRemainingTime(
+          (prevRemainingTime) =>
+            prevRemainingTime > 0 ? prevRemainingTime - 1 : 0 //reduces the second by 1
         );
       }, 1000);
     }
@@ -30,15 +35,13 @@ function HomePage() {
     };
   }, [isIdle, showModal]);
 
-
-
   useEffect(() => {
     if (remainingTime === 0 && showModal) {
       // alert("Time out!");
       setShowModal(false);
       navigate("/");
     }
-  }, [remainingTime, showModal, navigate]);// this is responsoble for logging user out after timer is down to zero and they have not clicked anything
+  }, [remainingTime, showModal, navigate]); // this is responsoble for logging user out after timer is down to zero and they have not clicked anything
 
   const handleLogOut = () => {
     setShowModal(false);
@@ -47,59 +50,43 @@ function HomePage() {
 
   const handleStayLoggedIn = () => {
     setShowModal(false);
-    setRemainingTime(30);
-  };
-  //click loggedin and gives them a timer of 30 seconds before rendered inactive again
 
-  // function millisToMinutesAndSeconds(millis) {
-  //   var minutes = Math.floor(millis / 60000);
-  //   var seconds = ((millis % 60000) / 1000).toFixed(0);
-  //   return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-  // }
+  };
 
   function millisToMinutesAndSeconds(millis) {
     var minutes = Math.floor(millis / 60000);
     var seconds = ((millis % 60000) / 1000).toFixed(0);
     return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
   }
-  
 
   return (
     <>
-      {/* handle isIdle for the content page */}
-      {isIdle && (
-        <>
-          <div>
-            <h1>Welcome to the Home Page</h1>
-            <p>Thank you for logging in!</p>
-          </div>
-        </>
-      )}
-
       {/* handle isIdle for the modal */}
-      {isIdle && showModal ? (
+      {isIdle && showModal && (
         <div className="modal">
           <div className="modal-content">
             <h2>Idle Timeout Warning</h2>
             <p>You are about to be logged out due to inactivity.</p>
             <br />
             Time remaining: {millisToMinutesAndSeconds(remainingTime * 1000)}
-
             <br />
-            <button className="btn btn-primary" onClick={handleLogOut}>
+            <div className="row">
+            <button className="btn btn-danger" onClick={handleLogOut}>
               Logout
             </button>
-            <button className="btn btn-light" onClick={handleStayLoggedIn}>
+            <button className="btn btn-primary " onClick={handleStayLoggedIn}>
               Stay Logged In
             </button>
+            </div>
+
           </div>
         </div>
-      ) : (
-        <div>
-          <h1>Welcome to the Home Page</h1>
-          <p>Thank you for logging in!</p>
-        </div>
       )}
+
+      <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>Thank you for logging in!</p>
+      </div>
     </>
   );
 }
--- src/Hooks/useIdleTimer.js
+++ src/Hooks/useIdleTimer.js
@@ -1,17 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useIdleTimer } from "react-idle-timer";
 
-function useIdle({ onIdle, idleTime = 1 }) {
+function useIdle({ onIdle, idleTime }) {
   const [isIdle, setIsIdle] = useState(null);
 
   //handles what happens when the user is idle
   const handleOnIdle = (event) => {
     setIsIdle(true); //set the state to true
-    const currentTime = new Date().getTime();
+    const currentTime = new Date();
+    const formattedCurrentTime = currentTime.toLocaleString("en-US", {
+      weekday: "short",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      timeZoneName: "short",
+    });
 
-    console.log("user is idle?", event); //log the user is idle followed by the event
-    console.log("Last Active time", getLastActiveTime());
-    console.log("Current time", currentTime); // you the log the time the user was last active
+    console.log("user is idle", event); //log the user is idle followed by the event
+    console.log("Last Active time", getLastActiveTime()); // you the log the time the user was last active
+    console.log("Current time", formattedCurrentTime); //gets the curent time maybe to see what
     onIdle(); //then call onIdle function
   };
 
